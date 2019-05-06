d0bc00c3372eb76b04c39444d3e01d89a9f83624
DerekHs <hsiaoderek@berkeley.edu>
2019-05-05 17:25:35 -0700

    finished pulling from firebase

---
+++ Complekshun/Controllers/PhotoGridController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoGridController.swift
+//  Complekshun
+//
+//  Created by Derek Hsiao on 5/2/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import Foundation
---
+++ Complekshun/Model/UserModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserModel.swift
+//  Complekshun
+//
+//  Created by Derek Hsiao on 5/2/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import Foundation
